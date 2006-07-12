16a45b2a2a1b5db9d1afd8ffa44bdde5856ef848
Michael Bayne <mdb@threerings.net>
2006-07-12 16:50:38 +0000

    Nixed unwanted extra blank line.

--- src/java/com/threerings/getdown/launcher/Downloader.java
+++ src/java/com/threerings/getdown/launcher/Downloader.java
@@ -18,7 +18,6 @@
 // this program; if not, write to the: Free Software Foundation, Inc.,
 // 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
 
-
 package com.threerings.getdown.launcher;
 
 import java.io.File;
