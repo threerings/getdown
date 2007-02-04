47e3bec864474765cdea49713363928d86d10b58
Michael Bayne <mdb@threerings.net>
2007-02-04 00:16:29 +0000

    Created ant task for generating signed applet parameters.

--- src/java/com/threerings/getdown/tools/AppletParamSigner.java
+++ src/java/com/threerings/getdown/tools/AppletParamSigner.java
@@ -1,3 +1,23 @@
+//
+// $Id$
+//
+// Getdown - application installer, patcher and launcher
+// Copyright (C) 2004-2006 Three Rings Design, Inc.
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 2 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+// more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program; if not, write to the: Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+
 package com.threerings.getdown.tools;
 
 import java.io.BufferedInputStream;
@@ -9,8 +29,8 @@ import java.security.Signature;
 import org.apache.commons.codec.binary.Base64;
 
 /**
- * Produces a signed hash of the appbase, appname, and image path to ensure
- * that signed copies of Getdown are not hijacked to run malicious code.
+ * Produces a signed hash of the appbase, appname, and image path to ensure that signed copies of
+ * Getdown are not hijacked to run malicious code.
  */
 public class AppletParamSigner
 {
@@ -18,12 +38,11 @@ public class AppletParamSigner
     {
         try {
             if (args.length != 7) {
-                System.err.println(
-                    "AppletParamSigner keystore storepass alias keypass " +
-                    "appbase appname imgpath"
-                );
+                System.err.println("AppletParamSigner keystore storepass alias keypass " +
+                                   "appbase appname imgpath");
                 System.exit(255);
             }
+
             String keystore = args[0];
             String storepass = args[1];
             String alias = args[2];
@@ -35,25 +54,20 @@ public class AppletParamSigner
 
             KeyStore store = KeyStore.getInstance("JKS");
             store.load(new BufferedInputStream(new FileInputStream(keystore)),
-                storepass.toCharArray());
-            PrivateKey key = (PrivateKey)store.getKey(alias,
-                keypass.toCharArray());
+                       storepass.toCharArray());
+            PrivateKey key = (PrivateKey)store.getKey(alias, keypass.toCharArray());
             Signature sig = Signature.getInstance("SHA1withRSA");
             sig.initSign(key);
             sig.update(params.getBytes());
             String signed = new String(Base64.encodeBase64(sig.sign()));
-            System.out.println(
-                "<param name=\"appbase\" value=\"" + appbase + "\" />");
-            System.out.println(
-                "<param name=\"appname\" value=\"" + appname + "\" />");
-            System.out.println(
-                "<param name=\"bgimage\" value=\"" + imgpath + "\" />");
-            System.out.println(
-                "<param name=\"signature\" value=\"" + signed + "\" />");
+            System.out.println("<param name=\"appbase\" value=\"" + appbase + "\" />");
+            System.out.println("<param name=\"appname\" value=\"" + appname + "\" />");
+            System.out.println("<param name=\"bgimage\" value=\"" + imgpath + "\" />");
+            System.out.println("<param name=\"signature\" value=\"" + signed + "\" />");
+
         } catch (Exception e) {
             System.err.println("Failed to produce signature.");
             e.printStackTrace();
         }
     }
-
 }
