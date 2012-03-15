828a4638b0e736be63f4e0c13fb660b16b580598
Ray Greenwell <ray@threerings.net>
2012-03-15 21:34:32 +0000

    This might be causing a divide by 0 error because all the sizes are 0...

--- src/main/java/com/threerings/getdown/util/MetaProgressObserver.java
+++ src/main/java/com/threerings/getdown/util/MetaProgressObserver.java
@@ -48,7 +48,9 @@ public class MetaProgressObserver implements ProgressObserver
     // documentation inherited from interface
     public void progress (int percent)
     {
-        _target.progress((int)((_accum + (percent * _elementSize)) / _totalSize));
+        if (_elementSize > 0) {
+            _target.progress((int)((_accum + (percent * _elementSize)) / _totalSize));
+        }
     }
 
     protected ProgressObserver _target;
