8e446d96e44e4c52f517f11e2565a3384d9b50d7
Michael Bayne <mdb@samskivert.com>
2015-02-16 09:46:46 -0800

    Whitespace tweaks.

--- src/main/java/com/threerings/getdown/data/SysProps.java
+++ src/main/java/com/threerings/getdown/data/SysProps.java
@@ -24,13 +24,13 @@ public class SysProps
     }
 
     /** Configures the appbase (in lieu of providing a skeleton getdown.txt, and as a last resort
-     * fallback). Usage: {@code -Dappbase=someurl}. */
+      * fallback). Usage: {@code -Dappbase=someurl}. */
     public static String appBase () {
         return System.getProperty("appbase");
     }
 
     /** If true, disables redirection of logging into {@code launcher.log}.
-     * Usage: {@code -Dno_log_redir}. */
+      * Usage: {@code -Dno_log_redir}. */
     public static boolean noLogRedir () {
         return System.getProperty("no_log_redir") != null;
     }
@@ -41,20 +41,20 @@ public class SysProps
     }
 
     /** If true, Getdown installs the app without ever bringing up a UI, except in the event of an
-     * error. NOTE: it does not launch the app. See {@link #launchInSilent}.
-     * Usage: {@code -Dsilent}. */
+      * error. NOTE: it does not launch the app. See {@link #launchInSilent}.
+      * Usage: {@code -Dsilent}. */
     public static boolean silent () {
         return System.getProperty("silent") != null;
     }
 
     /** If true, Getdown installs the app without ever bringing up a UI and then launches it.
-     * Usage: {@code -Dsilent=launch}. */
+      * Usage: {@code -Dsilent=launch}. */
     public static boolean launchInSilent () {
         return "launch".equals(System.getProperty("silent"));
     }
 
     /** Specifies the a delay (in minutes) to wait before starting the update and install process.
-     * Usage: {@code -Ddelay=N}. */
+      * Usage: {@code -Ddelay=N}. */
     public static int startDelay () {
         return Integer.getInteger("delay", 0);
     }
@@ -65,17 +65,17 @@ public class SysProps
     }
 
     /** If true, Getdown will run the application in the same VM in which Getdown is running. If
-     * false (the default), Getdown will fork a new VM. Note that reusing the same VM prevents
-     * Getdown from configuring some launch-time-only VM parameters (like -mxN etc.).
-     * Usage: {@code -Ddirect}. */
+      * false (the default), Getdown will fork a new VM. Note that reusing the same VM prevents
+      * Getdown from configuring some launch-time-only VM parameters (like -mxN etc.).
+      * Usage: {@code -Ddirect}. */
     public static boolean direct () {
         return Boolean.getBoolean("direct");
     }
 
     /** Specifies the connection timeout (in seconds) to use when downloading control files from
-     * the server. This is chiefly useful when you are running in versionless mode and want Getdown
-     * to more quickly timeout its startup update check if the server with which it is
-     * communicating is not available. Usage: {@code -Dconnect_timeout=N}. */
+      * the server. This is chiefly useful when you are running in versionless mode and want Getdown
+      * to more quickly timeout its startup update check if the server with which it is
+      * communicating is not available. Usage: {@code -Dconnect_timeout=N}. */
     public static int connectTimeout () {
         return Integer.getInteger("connect_timeout", 0);
     }
