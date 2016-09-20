5690570f93c62241ce33d7838d6c7b6e11c638b1
Thomas Küstermann <thokuest@outlook.com>
2016-09-20 13:50:52 +0200

    increase the "sleep time"

--- src/test/java/com/threerings/getdown/classpath/cache/ResourceCacheTest.java
+++ src/test/java/com/threerings/getdown/classpath/cache/ResourceCacheTest.java
@@ -49,7 +49,7 @@ public class ResourceCacheTest
     {
         File cachedFile = cacheFile();
 
-        TimeUnit.MILLISECONDS.sleep(5);
+        TimeUnit.MILLISECONDS.sleep(50);
 
         // caching it another time
         File sameCachedFile = cacheFile();
@@ -62,7 +62,7 @@ public class ResourceCacheTest
     {
         cacheFile();
 
-        TimeUnit.MILLISECONDS.sleep(5);
+        TimeUnit.MILLISECONDS.sleep(50);
 
         // caching it another time
         File sameCachedFile = cacheFile();
