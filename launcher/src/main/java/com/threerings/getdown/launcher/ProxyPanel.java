5eaab569ac8dfdbb2691fa7cb6f31a6355636fe4
Thomas Gehrig <tgehrig@ai-ag.de>
2018-11-06 16:46:58 +0100

    Added credentials dialogs in proxyPanel

--- launcher/src/main/java/com/threerings/getdown/launcher/ProxyPanel.java
+++ launcher/src/main/java/com/threerings/getdown/launcher/ProxyPanel.java
@@ -5,140 +5,226 @@
 
 package com.threerings.getdown.launcher;
 
+import static com.threerings.getdown.Log.log;
+
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 import com.samskivert.swing.GroupLayout;
 import com.samskivert.swing.Spacer;
 import com.samskivert.swing.VGroupLayout;
-
 import com.threerings.getdown.util.MessageUtil;
-import static com.threerings.getdown.Log.log;
 
 /**
  * Displays an interface with which the user can configure their proxy
  * settings.
  */
-public final class ProxyPanel extends JPanel
-    implements ActionListener
+public final class ProxyPanel extends JPanel implements ActionListener
 {
-    public ProxyPanel (Getdown getdown, ResourceBundle msgs)
+  public ProxyPanel(Getdown getdown, ResourceBundle msgs)
+  {
+    _getdown = getdown;
+    _msgs = msgs;
+
+    setLayout(new VGroupLayout());
+    setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+    add(new SaneLabelField(get("m.configure_proxy")));
+    add(new Spacer(5, 5));
+
+    JPanel row = new JPanel(new GridLayout());
+    row.add(new SaneLabelField(get("m.proxy_host")), BorderLayout.WEST);
+    row.add(_host = new SaneTextField());
+    add(row);
+
+    row = new JPanel(new GridLayout());
+    row.add(new SaneLabelField(get("m.proxy_port")), BorderLayout.WEST);
+    row.add(_port = new SaneTextField());
+    add(row);
+
+    add(new Spacer(5, 5));
+
+    row = new JPanel(new GridLayout());
+    row.add(new SaneLabelField(get("m.proxy_auth_required")), BorderLayout.WEST);
+    JCheckBox tCheckBox = new JCheckBox();
+    row.add(tCheckBox);
+    add(row);
+
+    row = new JPanel(new GridLayout());
+    row.add(new SaneLabelField(get("m.proxy_username")), BorderLayout.WEST);
+    _username = new SaneTextField();
+    _username.setEnabled(false);
+    row.add(_username);
+    add(row);
+
+    row = new JPanel(new GridLayout());
+    row.add(new SaneLabelField(get("m.proxy_password")), BorderLayout.WEST);
+    _password = new SanePasswordField();
+    _password.setEnabled(false);
+    row.add(_password);
+    add(row);
+
+    tCheckBox.addItemListener(_itemListener);
+
+    add(new Spacer(5, 5));
+    add(new SaneLabelField(get("m.proxy_extra")));
+
+    row = GroupLayout.makeButtonBox(GroupLayout.CENTER);
+    JButton button;
+    row.add(button = new JButton(get("m.proxy_ok")));
+    button.setActionCommand("ok");
+    button.addActionListener(this);
+    row.add(button = new JButton(get("m.proxy_cancel")));
+    button.setActionCommand("cancel");
+    button.addActionListener(this);
+    add(row);
+
+    // set up any existing proxy defaults
+    String host = System.getProperty("http.proxyHost");
+    if (host != null)
     {
-        _getdown = getdown;
-        _msgs = msgs;
-
-        setLayout(new VGroupLayout());
-        setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-        add(new JLabel(get("m.configure_proxy")));
-        add(new Spacer(5, 5));
-
-        JPanel row = new JPanel(new BorderLayout(5, 5));
-        row.add(new JLabel(get("m.proxy_host")), BorderLayout.WEST);
-        row.add(_host = new SaneTextField());
-        add(row);
-
-        row = new JPanel(new BorderLayout(5, 5));
-        row.add(new JLabel(get("m.proxy_port")), BorderLayout.WEST);
-        row.add(_port = new SaneTextField());
-        add(row);
-
-        add(new Spacer(5, 5));
-        add(new JLabel(get("m.proxy_extra")));
-
-        row = GroupLayout.makeButtonBox(GroupLayout.CENTER);
-        JButton button;
-        row.add(button = new JButton(get("m.proxy_ok")));
-        button.setActionCommand("ok");
-        button.addActionListener(this);
-        row.add(button = new JButton(get("m.proxy_cancel")));
-        button.setActionCommand("cancel");
-        button.addActionListener(this);
-        add(row);
-
-        // set up any existing proxy defaults
-        String host = System.getProperty("http.proxyHost");
-        if (host != null) {
-            _host.setText(host);
-        }
-        String port = System.getProperty("http.proxyPort");
-        if (port != null) {
-            _port.setText(port);
-        }
+      _host.setText(host);
     }
+    String port = System.getProperty("http.proxyPort");
+    if (port != null)
+    {
+      _port.setText(port);
+    }
+  }
 
-    // documentation inherited
+  // documentation inherited
+  @Override
+  public void addNotify()
+  {
+    super.addNotify();
+    _host.requestFocusInWindow();
+  }
+
+  // documentation inherited
+  @Override
+  public Dimension getPreferredSize()
+  {
+    // this is annoyingly hardcoded, but we can't just force the width
+    // or the JLabel will claim a bogus height thinking it can lay its
+    // text out all on one line which will booch the whole UI's
+    // preferred size
+    return new Dimension(500, 450);
+  }
+
+  // documentation inherited from interface
+  @Override
+  public void actionPerformed(ActionEvent e)
+  {
+    String cmd = e.getActionCommand();
+    if (cmd.equals("ok"))
+    {
+      // communicate this info back to getdown
+      _getdown.configureProxy(_host.getText(), _port.getText());
+
+    }
+    else
+    {
+      // they canceled, we're outta here
+      System.exit(0);
+    }
+  }
+
+  /** Used to look up localized messages. */
+  protected String get(String key)
+  {
+    // if this string is tainted, we don't translate it, instead we
+    // simply remove the taint character and return it to the caller
+    if (MessageUtil.isTainted(key))
+    {
+      return MessageUtil.untaint(key);
+    }
+    try
+    {
+      return _msgs.getString(key);
+    }
+    catch (MissingResourceException mre)
+    {
+      log.warning("Missing translation message '" + key + "'.");
+      return key;
+    }
+  }
+
+  protected static class SaneTextField extends JTextField
+  {
     @Override
-    public void addNotify  ()
+    public Dimension getPreferredSize()
     {
-        super.addNotify();
-        _host.requestFocusInWindow();
+      Dimension d = super.getPreferredSize();
+      d.width = Math.max(d.width, 150);
+      return d;
     }
+  }
 
-    // documentation inherited
+  protected static class SanePasswordField extends JPasswordField
+  {
     @Override
-    public Dimension getPreferredSize ()
+    public Dimension getPreferredSize()
     {
-        // this is annoyingly hardcoded, but we can't just force the width
-        // or the JLabel will claim a bogus height thinking it can lay its
-        // text out all on one line which will booch the whole UI's
-        // preferred size
-        return new Dimension(500, 350);
+      Dimension d = super.getPreferredSize();
+      d.width = Math.max(d.width, 150);
+      return d;
+    }
+  }
+
+  protected static class SaneLabelField extends JLabel
+  {
+    public SaneLabelField(String message)
+    {
+      super(message);
     }
 
-    // documentation inherited from interface
-    public void actionPerformed (ActionEvent e)
+    @Override
+    public Dimension getPreferredSize()
     {
-        String cmd = e.getActionCommand();
-        if (cmd.equals("ok")) {
-            // communicate this info back to getdown
-            _getdown.configureProxy(_host.getText(), _port.getText());
-
-        } else {
-            // they canceled, we're outta here
-            System.exit(0);
-        }
+      Dimension d = super.getPreferredSize();
+      d.width = Math.max(d.width, 200);
+      return d;
     }
+  }
 
-    /** Used to look up localized messages. */
-    protected String get (String key)
+  protected ItemListener _itemListener = new ItemListener()
+  {
+
+    @Override
+    public void itemStateChanged(ItemEvent aE)
     {
-        // if this string is tainted, we don't translate it, instead we
-        // simply remove the taint character and return it to the caller
-        if (MessageUtil.isTainted(key)) {
-            return MessageUtil.untaint(key);
-        }
-        try {
-            return _msgs.getString(key);
-        } catch (MissingResourceException mre) {
-            log.warning("Missing translation message '" + key + "'.");
-            return key;
-        }
+      if (aE.getStateChange() == ItemEvent.SELECTED)
+      {
+        _username.setEnabled(true);
+        _password.setEnabled(true);
+      }
+      else
+      {
+        _username.setEnabled(false);
+        _password.setEnabled(false);
+      }
     }
+  };
 
-    protected static class SaneTextField extends JTextField
-    {
-        @Override
-        public Dimension getPreferredSize () {
-            Dimension d = super.getPreferredSize();
-            d.width = Math.max(d.width, 150);
-            return d;
-        }
-    }
+  protected Getdown _getdown;
+  protected ResourceBundle _msgs;
 
-    protected Getdown _getdown;
-    protected ResourceBundle _msgs;
-
-    protected JTextField _host;
-    protected JTextField _port;
+  protected JTextField _host;
+  protected JTextField _port;
+  protected JTextField _username;
+  protected JPasswordField _password;
 }
