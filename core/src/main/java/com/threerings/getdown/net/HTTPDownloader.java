9402d7c900808c6ac97c44ff4a39d1fc7143c8d9
Michael Bayne <mdb@samskivert.com>
2018-12-06 11:55:49 -0800

    Update copyright headers.

--- core/src/main/java/com/threerings/getdown/net/HTTPDownloader.java
+++ core/src/main/java/com/threerings/getdown/net/HTTPDownloader.java
@@ -1,6 +1,6 @@
 //
 // Getdown - application installer, patcher and launcher
-// Copyright (C) 2004-2016 Getdown authors
+// Copyright (C) 2004-2018 Getdown authors
 // https://github.com/threerings/getdown/blob/master/LICENSE
 
 package com.threerings.getdown.net;
