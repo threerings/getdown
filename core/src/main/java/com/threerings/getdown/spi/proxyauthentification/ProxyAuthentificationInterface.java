d1d4269e24ac85dcaacea1b8a09c587312b6bd7c
Thomas Gehrig <tgehrig@ai-ag.de>
2018-12-14 10:59:46 +0100

    removed copyright disclaimers

--- core/src/main/java/com/threerings/getdown/spi/proxyauthentification/ProxyAuthentificationInterface.java
+++ core/src/main/java/com/threerings/getdown/spi/proxyauthentification/ProxyAuthentificationInterface.java
@@ -1,16 +1,3 @@
-/*
- * ProxyAuthentificationInterface.java
- *
- * Copyright (c) 2018 Administration Intelligence AG,
- * Steinbachtal 2b, 97082 Wuerzburg, Germany.
- * All rights reserved.
- *
- * This software is the confidential and proprietary information
- * of Administration Intelligence AG ("Confidential Information").
- * You shall not disclose such Confidential Information and shall use
- * it only in accordance with the terms of the license agreement you
- * entered into with Administration Intelligence.
- */
 
 package com.threerings.getdown.spi.proxyauthentification;
 
