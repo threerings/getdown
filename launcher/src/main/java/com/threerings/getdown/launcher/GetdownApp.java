8d1ccf32dc1a2839efec58ca0791ba857734db5d
sergiorussia <22471371+sergiorussia@users.noreply.github.com>
2019-04-06 17:49:06 +0300

    support no-arg launch for standalone launcher

--- launcher/src/main/java/com/threerings/getdown/launcher/GetdownApp.java
+++ launcher/src/main/java/com/threerings/getdown/launcher/GetdownApp.java
@@ -43,6 +43,17 @@ public class GetdownApp
      * The main entry point of the Getdown launcher application.
      */
     public static void main (String[] argv) {
+        if (argv.length == 0) {
+            // log object should not be used here yet, otherwise it will be initialized incorrectly
+            try {
+                final File jarFile = new File(GetdownApp.class.getProtectionDomain().getCodeSource().getLocation().toURI());
+                String jarDir = jarFile.getParent();
+                argv = new String[]{jarDir};
+                System.out.printf("No args passed, running from current dir. [current dir=%s]\n", jarDir);
+            } catch (Exception e) {
+                System.err.printf("No args passed, failed to fallback to current dir. %s\n", e);
+            }
+        }
         try {
             start(argv);
         } catch (Exception e) {
